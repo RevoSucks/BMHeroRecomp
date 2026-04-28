3490b9a8886b9e31f54750a843445e47707c09fa
RevoSucks <projectrevotpp@hotmail.com>
2026-04-28 18:33:20 -0400

    fix func_800BFAE0 crash

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -64,7 +64,7 @@
 
 #include "../../lib/rt64/src/contrib/stb/stb_image.h"
 
-const std::string version_string = "0.6.1";
+const std::string version_string = "0.6.2";
 
 template<typename... Ts>
 void exit_error(const char* str, Ts ...args) {
